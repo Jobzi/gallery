bb746aa3560b2675f4c3a8391e27f7c077b91aa8
Jipson Murillo <jipsonmurillo@gmail.com>
2022-01-30 18:27:15 -0500

    fix: render images from getstaticsprops

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -3,7 +3,7 @@ import { Polaroid } from '../../components/Polaroid'
 import HeadSeo from '../../components/Seo'
 import { fadeAnimation } from '../../styles/variants'
 
-export default function Gallery () {
+export default function Gallery ({ data }) {
   return (
     <>
       <HeadSeo section='Demo'/>
@@ -24,11 +24,24 @@ export default function Gallery () {
       </motion.div>
       <section className='grid place-items-center mt-4 mb-5'>
         <div className='flex flex-wrap'>
-          <Polaroid src={'https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg'} alt={'img 1'} width={'30'}/>
-          <Polaroid src={'https://image.ibb.co/crFarc/pexels_photo_100756.jpg'} alt={'img 1'} width={'30'}/>
-          <Polaroid src={'https://image.ibb.co/hQaarc/antique_blur_camera_828378.jpg'} alt={'img 1'} width={'30'}/>
+          {data.map((node) => (
+              <Polaroid key={node.image} src={node.image} legend={node.title} alt={'img 1'} width={'30'}/>
+          ))}
         </div>
       </section>
     </>
   )
+}
+
+export async function getStaticProps () {
+  const data = [
+    { image: 'https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg', title: 'London' },
+    { image: 'https://image.ibb.co/crFarc/pexels_photo_100756.jpg', title: 'Florianopolis' },
+    { image: 'https://image.ibb.co/hQaarc/antique_blur_camera_828378.jpg', title: 'Camera' }
+  ]
+  return {
+    props: {
+      data: data
+    }
+  }
 }
